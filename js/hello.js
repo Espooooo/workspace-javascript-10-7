49dcbb4c86bdc98dfa84c7c999d288f62537cc79
Espooooo <espo_brendon@yahoo.com>
2020-10-07 13:13:41 -0400

    added time

--- js/hello.js
+++ js/hello.js
@@ -1,1 +1,14 @@
 console.log("I am in hello.js")
+
+var today = new Date();
+var hoursNow = today.getHours();
+
+
+var greetings = "";
+if (hoursNow <= 12){
+  greetings = "Good Morning";
+}else{
+  grettings = "Good Afternoob";
+}
+
+document.write(greetings);
