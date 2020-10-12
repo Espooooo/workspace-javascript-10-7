0e8b1dcacc4c476905edf03b4e689d8ed9c3044f
Espooooo <espo_brendon@yahoo.com>
2020-10-12 13:13:30 -0400

    finsihed

---
+++ js/arith.js
@@ -0,0 +1,8 @@
+// get ele
+
+//var num1 = parseInt(document.getElementById(n1).textContent);
+//var num2 = parseInt(document.getElementById(n2).textContent);
+
+//console.log(num1)
+
+//var c = num1 + num2;
